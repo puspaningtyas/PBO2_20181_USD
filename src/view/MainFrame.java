78c6081cbf7a666f1f14a6e70e72656b26fb2d25
jarkom <jarkom@jarkom-C00>
2018-09-07 12:33:39 +0700

    kelas frame dicoba

---
+++ src/view/MainFrame.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import java.awt.HeadlessException;
+import java.awt.MenuBar;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+/**
+ *
+ * @author jarkom
+ */
+public class MainFrame extends JFrame {
+
+    private JMenuBar menuBar;
+    private JMenu fileMenu;
+    private JMenuItem exitMenuItem;
+
+    public MainFrame() throws HeadlessException {
+        init();
+    }
+
+    public void init() {
+        // buat menu bar
+        menuBar = new JMenuBar();
+        // set Titile
+        this.setTitle("Main Frame");
+        // buat menu
+        fileMenu = new JMenu("File");
+        exitMenuItem = new JMenuItem("exit");
+        fileMenu.add(exitMenuItem);
+        menuBar.add(fileMenu);
+        
+        this.setJMenuBar(menuBar);
+    }
+
+}
