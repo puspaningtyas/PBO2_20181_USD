d6e967ee43f2947c04bcabef159e1be3a3b13c5c
jarkom <jarkom@jarkom-C00>
2018-09-14 11:54:27 +0700

    kelas frame dicoba

---
+++ src/testPraktikum12September2018/TestMainFrame.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testPraktikum12September2018;
+
+import testView.*;
+import view.LatihanDialog;
+import view.MainFrame;
+
+/**
+ *
+ * @author jarkom
+ */
+public class TestMainFrame {
+    public static void main(String[] args) {
+        MainFrame test = new MainFrame();
+        test.setSize(300, 400);
+        test.setVisible(true);
+    }
+   
+}
